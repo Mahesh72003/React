e7655a1375747432c76804e3bbaa59c56067c2ce
Mahesh72003 <maheshvanjre03@gmail.com>
2025-12-23 10:25:33 +0530

    Add functional components: Title and Header

--- App.js
+++ App.js
@@ -5,20 +5,36 @@ import ReactDOM from "react-dom/client";
 const style={
     backgroundColor:"green"
 }
-const App = (
+
+const Title = () => { 
+return (
     <div>
-        <div style={{backgroundColor:"red"}}>
+        <div>
             <h3>Hello this is from React</h3>
         </div>
-        
-        <h1 style={style}>this is from react</h1>
+        <div>
+            <p>this is from react</p>
+        </div>
     </div>
 );
+};
+
+const Header = () => {
+    return (
+        <div style={{ backgroundColor: "red" }} className="SecondDiv">
+            <div>
+                <Title />
+                <h1 style={style}>this is from react</h1>
+            </div>
+        </div>
+    );
+};
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(App);
+root.render(<Header/>);
 
 
 
